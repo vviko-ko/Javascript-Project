4b26bca17bcf405b3fb809724da70c4722562251
vviko-ko <olusanmiv@gmail.com>
2023-11-20 23:08:04 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const colors = ['red','green','rgba(133,122,200)','#f15025'];
+const colors = ['Red','Green','rgba(133,122,200)','#f15025'];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 btn.addEventListener('click', function(){
